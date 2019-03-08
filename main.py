a0e828cb72d19669e1287cab0535d12be208af7d
Audrey Corbeil Therrien <therria@slac.stanford.edu>
2019-03-08 10:33:21 -0800

    Basic working examples pulled from https://stackoverflow.com/questions/12459811/how-to-embed-matplotlib-in-pyqt-for-dummies

---
+++ main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a small tool to help people learn, visualize  and understand how 
+convolution operates by reconstructing the convolution integral graphically.
+
+Ce programme est un outil visant à aider l'apprentissage, la visualisation et 
+la compréhension de la convolution en contruisant graphiquement l'intégral de
+convolution.
+
+License : GPL 3
+"""
+
+import numpy
+import sys
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
+import random
+
+__author__ = "Audrey Corbeil Therrien"
+__copyright__ = '2019, ConstructionConvolution'
+__credits__ = ["Audrey Corbeil Therrien"]
+__license__ = "GPL 3"
+__version__ = "0.0.1"
+__maintainer__ = "Audrey Corbeil Therrien"
+__email__ = "therria@stanford.edu"
+__status__ = "Prototype"
+
+
+class Window(QDialog):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # Just some button connected to `plot` method
+        self.button = QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+
+        # set the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+    def plot(self):
+        ''' plot some random stuff '''
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # instead of ax.hold(False)
+        self.figure.clear()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        ax.plot(data, '*-')
+
+        # refresh canvas
+        self.canvas.draw()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    main = Window()
+    main.show()
+
+    sys.exit(app.exec_())
+    
